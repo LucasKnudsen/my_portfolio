c3d6b8d77b0bc270014730b564db4064a7a396f1
Lucas <laksteelhouse@gmail.com>
2021-01-27 11:56:16 +0100

    Structure the 3 main components (Header, Hello, Footer). Starting to style

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import Hello from './Hello'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Hello />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <h1 id="footer">Made with React {React.version}</h1>
+        </div>
+    )
+}
+
+export default Footer
