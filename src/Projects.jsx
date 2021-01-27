ab6be6583ab4a63588615f12a8c74591adc0bfa4
Lucas <laksteelhouse@gmail.com>
2021-01-27 21:08:43 +0100

    Implementing project list to be rendered in Container. List is received from .json and gotten with axios

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Container } from "semantic-ui-react"
+import axios from "axios"
 
-const Projects = () => {
-    return (
-        <Container>
-            <h1 id="projects-header">My Projects</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Eum, officia iusto quidem similique nostrum excepturi. 
-                Ipsum culpa rem illo at accusamus neque autem reprehenderit qui? 
-                Fugiat fugit expedita architecto blanditiis!
-            </p>
-        </Container>
-    )
+
+class Projects extends Component {
+    state = {
+        projects: []
+    };
+
+    componentDidMount() {
+        axios.get("./data/projects.json").then((response) => {
+            this.setState({ projects: response.data });
+        });
+    }
+
+    render() {
+        const { projects } = this.state;
+        
+        let projectsList = projects.map((project) => {
+            return (
+                <div id={`project-${project.id}`} key={project.id}>
+                    <h3>{project.name}</h3>
+
+                </div>
+            );
+        });
+
+        return ( 
+            <Container>
+                <h1 id="projects-header">My Projects</h1>
+                {projectsList}
+            </Container>
+        );
+    }    
 }
 
 export default Projects
